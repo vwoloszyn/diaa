5c66737ffea1f3191d13a3b4329e29b7e7a58f60
viniciuswoloszyn <woloszyn@tu-berlin.de>
2019-10-11 23:18:39 +0200

    fix

--- diaa/maker.py
+++ diaa/maker.py
@@ -1,7 +1,9 @@
 import json
 from nltk import agreement
+from urllib import request, parse
+import json,urllib.request
 
-def read_json(fn):
+def get_docs_from_json(fn):
     lines=[]
     fh = open(fn)
     for line in fh:
@@ -10,43 +12,80 @@ def read_json(fn):
     fh.close()
     return lines
 
-def get_labels(json):
+
+def get_docs_from_doccano(url_,project_):
+
+    ##getting token
+    #http post http://tc.qu.tu-berlin.de/v1/auth-token username=vinicius password=nopassword
+
+    auth_url=url_+"/v1/auth-token"
+    #print (auth_url)
+    data = {'username': 'vinicius', 'password': 'nopassword'}
+    req = request.Request(auth_url)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    jsondata = json.dumps(data)
+    jsondataasbytes = jsondata.encode('utf-8')   # needs to be bytes
+    result = request.urlopen(req, jsondataasbytes).read()
+    token=json.loads(result)["token"]
+    header = {'Content-Type': 'application/json; charset=utf-8','Authorization': 'Token '+str(token)}
+    #print (token)
+
+    #http://tc.qu.tu-berlin.de/v1/projects/4/docs/download?q=json
+
+    data_url=url_+"/v1/projects/"+str(project_)+"/docs/download?q=json&format=json"
+    #print (data_url)
+    req =  request.Request(data_url, headers=header)
+    webURL = request.urlopen(req)
+
+    data=[]
+    for line in webURL:
+        data.append(json.loads(line.decode("utf-8")))
+
+    return data
+
+
+def get_labels(docs):
     labels_=[]
     spans_={}
     users_=[]
-    for i in range(len(json)):
-        for l in json[i]:
-            if ("annotations" in list(json[i].keys())):
-                text=json[i]["text"]
-                labels=json[i]["annotations"]
-                for label in labels:
-                    user_=label["user"]
-                    type_=str(label["label"])
-                    token_=str(i)+"_"+str(label["start_offset"])+"_"+str(label["end_offset"])
-                    if user_ not in users_:
-                        users_.append(user_)
-                    if token_ not in spans_.keys():
-                        spans_[token_]={}
-                    spans_[token_][user_]=type_
+    for i in range(len(docs)):
+        doc=docs[i]
+        if ("annotations" in list(doc.keys())):
+            text=doc["text"]
+            labels=doc["annotations"]
+            for label in labels:
+                user_=label["user"]
+                type_=str(label["label"])
+                token_=str(i)+"_"+str(label["start_offset"])+"_"+str(label["end_offset"])
+                if user_ not in users_:
+                    users_.append(user_)
+                if token_ not in spans_.keys():
+                    spans_[token_]={}
+                spans_[token_][user_]=type_
+        else:
+            print("did not find annotations in record "+str(i))
+
     for key, value in spans_.items():
         for u in users_:
             if (u in spans_[key].keys() ):
-                labels_.append([str(u),key, spans_[key][u] ])
+                labels_.append((str(u),key, int(spans_[key][u]) ))
             else:
-                labels_.append([str(u),key, "-1"])
+                labels_.append((str(u),key, 0))
     if len(labels_)==0:
         print ("no labels found")
+
+    print (labels_)
     return labels_
 
 
 
 
-def calc(file):
-    json = read_json(file)
-    d=get_labels(json)
+def calc(labels):
+    metrics={}
+    ratingtask = agreement.AnnotationTask(data=labels)
+    metrics["kappa"]=ratingtask.kappa()
+    metrics["fleiss"]=ratingtask.multi_kappa()
+    metrics["alpha"]=ratingtask.alpha()
+    metrics["scotts"]=ratingtask.pi()
+    return metrics
 
-    ratingtask = agreement.AnnotationTask(data=d)
-    print("kappa " +str(ratingtask.kappa()))
-    print("fleiss " + str(ratingtask.multi_kappa()))
-    print("alpha " +str(ratingtask.alpha()))
-    print("scotts " + str(ratingtask.pi()))
