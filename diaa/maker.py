18536c910278088b6de591a355c54235345f8add
viniciuswoloszyn <woloszyn@tu-berlin.de>
2019-10-12 14:43:05 +0200

    fix

--- diaa/maker.py
+++ diaa/maker.py
@@ -2,6 +2,8 @@ import json
 from nltk import agreement
 from urllib import request, parse
 import json,urllib.request
+import os
+import tempfile
 
 def get_docs_from_json(fn):
     lines=[]
@@ -13,14 +15,18 @@ def get_docs_from_json(fn):
     return lines
 
 
-def get_docs_from_doccano(url_,project_):
+def get_docs_from_doccano(url_):
 
+    host=url_[0]
+    user_=url_[1]
+    pass_=url_[2]
+    project_=url_[3]
     ##getting token
     #http post http://tc.qu.tu-berlin.de/v1/auth-token username=vinicius password=nopassword
 
-    auth_url=url_+"/v1/auth-token"
+    auth_url=host+"/v1/auth-token"
     #print (auth_url)
-    data = {'username': 'vinicius', 'password': 'nopassword'}
+    data = {'username': user_, 'password': pass_}
     req = request.Request(auth_url)
     req.add_header('Content-Type', 'application/json; charset=utf-8')
     jsondata = json.dumps(data)
@@ -32,7 +38,7 @@ def get_docs_from_doccano(url_,project_):
 
     #http://tc.qu.tu-berlin.de/v1/projects/4/docs/download?q=json
 
-    data_url=url_+"/v1/projects/"+str(project_)+"/docs/download?q=json&format=json"
+    data_url=host+"/v1/projects/"+str(project_)+"/docs/download?q=json&format=json"
     #print (data_url)
     req =  request.Request(data_url, headers=header)
     webURL = request.urlopen(req)
@@ -74,8 +80,77 @@ def get_labels(docs):
     if len(labels_)==0:
         print ("no labels found")
 
-    print (labels_)
+    #print (labels_)
     return labels_
+
+
+def docs_to_ann(docs):
+    import tempfile
+    import shutil
+    temp_dir = tempfile.mkdtemp()
+    #print(temp_dir)
+    labels_=[]
+    annotators_=[]
+    files_=[]
+    for i in range(len(docs)):
+        doc=docs[i]
+        if ("annotations" in list(doc.keys())):
+            text=doc["text"]
+            labels=doc["annotations"]
+            users_={}
+            for label in labels:
+                user_=label["user"]
+                type_=str(label["label"])
+                labels_.append(type_)
+                annotators_.append(str(user_))
+                start_=label["start_offset"]
+                end_=label["end_offset"]
+                if user_ not in users_.keys():
+                    users_[user_]=[]
+                nonono = "x" * int(int(end_)-int(start_))
+                users_[user_].append(["T"+str(len(users_[user_])+1),type_,start_,end_,nonono])
+
+        for u,values in users_.items():
+            dir_user=str(temp_dir)+"/"+str(u)
+            file_dir_user=dir_user+"/"+str(i)+".ann"
+            os.makedirs(dir_user, exist_ok=True)
+            #print (dir_user)
+            files_.append(str(i)+".ann")
+            with open(file_dir_user, 'w') as f:
+                for v in values:
+
+                    line=str(v[0])+"\t"+str(v[1])+" "+str(v[2])+" "+str(v[3])
+                    #print (line)
+                    f.write(line+"\n")
+            #print (file_dir_user)
+    return temp_dir,list(set(labels_)),list(set(annotators_)),list(set(files_))
+
+
+
+
+def compute_f1_scores(docs):
+    metrics={}
+
+    from bratiaa.agree import F1Agreement, partial, input_generator
+    project,labels,annotators,docs = docs_to_ann(docs)
+    #print (annotators)
+    #print (docs)
+    xx=F1Agreement(partial(input_generator, project), labels,annotators=annotators, documents=docs)
+
+    import bratiaa as biaa
+
+    biaa.iaa_report(xx)
+    #
+    #
+    # print (project)
+    # metrics["f1"]=xx.mean_sd_total()[0]
+    # print ("f1")
+    # print (xx.mean_sd_total()[0])
+    # print ("f1 per doc")
+    # print (xx.mean_sd_per_document())
+
+    return metrics
+
 
 
 
@@ -87,5 +162,6 @@ def calc(labels):
     metrics["fleiss"]=ratingtask.multi_kappa()
     metrics["alpha"]=ratingtask.alpha()
     metrics["scotts"]=ratingtask.pi()
+
     return metrics
 
--- main.py
+++ main.py
@@ -8,34 +8,39 @@ import operator
 if __name__ == "__main__":
     pparser = argparse.ArgumentParser()
     #pparser.add_argument('-a', nargs='+', help='annotators', required=False, default=[])
-    pparser.add_argument('-i', help='input', required=False, )
-    pparser.add_argument('-d', help='docs', action='store_true', required=False, )
-    pparser.add_argument('-u', help='url', required=False, )
-    pparser.add_argument('-p', help='project', required=False, )
+    pparser.add_argument('-i', help='input', required=False )
+    #pparser.add_argument('-d', help='docs', action='store_true', required=False, )
+    pparser.add_argument('-u', help='url', nargs='+', default=[], required=False, )
+    #pparser.add_argument('-p', help='project', required=False, )
 
 
     args = pparser.parse_args()
 
     if args.u :
-        docs = get_docs_from_doccano(args.u,args.p)
+        docs = get_docs_from_doccano(args.u)
     else:
         docs = get_docs_from_json(args.i)
 
-    if args.d:
-        doc_agree={}
-        for i in range(len(docs)):
-            doc=docs[i]
-            labels=get_labels([doc])
-            ag=calc(labels)
-            doc_agree[i]=ag["kappa"]
+    # if args.d:
+    #     doc_agree={}
+    #     for i in range(len(docs)):
+    #         doc=docs[i]
+    #         labels=get_labels([doc])
+    #         ag=calc(labels)
+    #         doc_agree[i]=ag["kappa"]
+    #
+    #     sorted_agreeemnt = sorted(doc_agree.items(), key=operator.itemgetter(1))
+    #     print (sorted_agreeemnt)
+    # else:
 
-        sorted_agreeemnt = sorted(doc_agree.items(), key=operator.itemgetter(1))
-        print (sorted_agreeemnt)
-    else:
-        labels=get_labels(docs)
-        #print (labels)
-        agreement=calc(labels)
-        print (agreement)
+    labels=get_labels(docs)
+    compute_f1_scores(docs)
+
+    agreement=calc(labels)
+    print (agreement)
+
+
+    #docs_to_ann(docs)
 
 
 
